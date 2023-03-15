977e33dc4ec6d5f3226829974d0e5d1342114e03
Martin Ngungu <ngushdare@gmail.com>
2023-03-15 13:07:58 +0300

    right html tags for build

--- pages/_document.js
+++ pages/_document.js
@@ -5,9 +5,8 @@ class MyDocument extends Document {
     return (
       <Html lang="en">
         <Head>
-          <meta charset="utf-8" />
-          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-          <title>Crystal Gift</title>
+          <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="description" content="Crystal GIFT" />
           <meta name="keywords" content="Gifts, present, sure, kenya, christmas, Crystal" />
@@ -23,39 +22,26 @@ class MyDocument extends Document {
           <meta name="twitter:url" content="" />
           <meta name="twitter:card" content="" /> 
           <link rel="shortcut icon" href="favicon.ico" />
-          <link rel="stylesheet" href="css/animate.css" />
-          <link rel="stylesheet" href="css/icomoon.css" />
-          <link rel="stylesheet" href="css/simple-line-icons.css" /> 
-          <link rel="stylesheet" href="css/bootstrap.css" /> 
-          <link rel="stylesheet" href="css/owl.carousel.min.css" />
-          <link rel="stylesheet" href="css/owl.theme.default.min.css" />
-          <link rel="stylesheet" href="css/style.css" />
-          <script src="js/modernizr-2.6.2.min.js"></script>
-          <script src="js/respond.min.js"></script>
-
-          
-	<script src="js/jquery.min.js"></script>
-	
-	<script src="js/jquery.easing.1.3.js"></script>
-	
-	<script src="js/bootstrap.min.js"></script>
-	
-	<script src="js/jquery.waypoints.min.js"></script>
-	
-	<script src="js/jquery.stellar.min.js"></script>
-	
-	<script src="js/owl.carousel.min.js"></script>
-	
-	<script src="js/jquery.countTo.js"></script>
-	
-	<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCefOgb1ZWqYtj7raVSmN4PL2WkTrc-KyA&sensor=false"></script>
-	<script src="js/google_map.js"></script>
-	
-	<script src="js/main.js"></script>
+          <link rel="stylesheet" href="/css/animate.css" />
+          <link rel="stylesheet" href="/css/icomoon.css" />
+          <link rel="stylesheet" href="/css/simple-line-icons.css" /> 
+          <link rel="stylesheet" href="/css/bootstrap.css" /> 
+          <link rel="stylesheet" href="/css/owl.carousel.min.css" />
+          <link rel="stylesheet" href="/css/owl.theme.default.min.css" />
+          <link rel="stylesheet" href="/css/style.css" />
+          <script src="/js/modernizr-2.6.2.min.js" async></script>
+          <script src="/js/respond.min.js" async></script>
+          <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
         </Head>
         <body>
           <Main />
           <NextScript />
+          <script src="/js/jquery.easing.1.3.js" defer></script>
+          <script src="/js/bootstrap.min.js" defer></script>
+          <script src="/js/jquery.waypoints.min.js" defer></script>
+          <script src="/js/jquery.stellar.min.js" defer></script>
+          <script src="/js/owl.carousel.min.js" defer></script>
+          <script src="/js/main.js" defer></script>
         </body>
       </Html>
     );
--- pages/component/allshops.js
+++ pages/component/allshops.js
@@ -3,53 +3,53 @@ import axios from 'axios';
 import Image from 'next/image';
 
 const Shops = () => {
-const [shops, setShops] = useState([]);
+  const [shops, setShops] = useState([]);
 
-useEffect(() => {
-axios.get('https://www.crystalgift.co.ke/hook/connect.php')
-.then(response => {
-setShops(response.data);
-})
-.catch(error => {
-console.log(error);
-});
-}, []);
+  useEffect(() => {
+    axios.get('https://www.crystalgift.co.ke/hook/connect.php')
+      .then(response => {
+        setShops(response.data);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }, []);
 
-return (
-<section id="fh5co-pricing" data-section="pricing">
-<div className="fh5co-pricing">
-<div className="container">
-<div className="row">
-<div className="col-md-12 text-center">
-<h2>Gift Shops</h2>
-<p>Showing {shops.length} shops</p>
-</div>
-</div>
-<div className="row">
-{shops.map(shop => (
-<div key={shop.id} className="col-md-3 col-sm-6">
-<div className="price-box to-animate">
-<div className="price"><sup className="currency"></sup></div>
-<p className="business_name">{shop.business_name}</p>
-<hr />
-<span className="business_logo">
-    <Image 
-    src={shop.business_logo} 
-    alt="Business Logo" 
-    width={200} 
-    height={200} 
-    />
-</span>
-<hr />
-<p><a href="#" className="btn btn-primary">Explore Gifts</a></p>
-</div>
-</div>
-))}
-</div>
-</div>
-</div>
-</section>
-);
+  return (
+    <section id="fh5co-pricing" data-section="pricing">
+      <div className="fh5co-pricing">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 text-center">
+              <h2>Gift Shops</h2>
+              <p>Showing {shops.length} shops</p>
+            </div>
+          </div>
+          <div className="row">
+            {shops.map(shop => (
+              <div key={shop.id} className="col-md-3 col-sm-6">
+                <div className="price-box to-animate">
+                  <div className="price"><sup className="currency"></sup></div>
+                  <p className="business_name">{shop.business_name}</p>
+                  <hr />
+                  <span className="business_logo">
+                    <Image 
+                      src={shop.business_logo} 
+                      alt="Business Logo" 
+                      width={200} 
+                      height={200} 
+                    />
+                  </span>
+                  <hr />
+                  <p><a href="#" className="btn btn-primary">Explore Gifts</a></p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Shops;
